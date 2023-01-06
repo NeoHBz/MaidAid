8fad7aa965b1f7a6fa84dcc962ae28dc5df9e3a9
janvi01 <janvibajo1@gmail.com>
2023-01-06 23:12:05 +0530

    [frontend]added: basic architecture

---
+++ frontend/src/layouts/LandingPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Heading, VStack } from "@chakra-ui/react";
+
+export default function LandingPage() {
+  return (
+    <VStack spacing={16}>
+      <Heading>Landing Page</Heading>
+    </VStack>
+  );
+}
