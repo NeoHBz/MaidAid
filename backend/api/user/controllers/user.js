f5bf1982a650b37de236101fb124f4fa5e1a9a81
NeoHBz <neohbz@gmail.com>
2023-01-07 13:41:47 +0530

    added user register controller

--- backend/api/user/controllers/user.js
+++ backend/api/user/controllers/user.js
@@ -1,4 +1,6 @@
 "use strict";
+const uniqid = require("uniqid");
+
 module.exports = {
   async login(ctx) {
     const { email, password } = ctx.request.body;
@@ -17,5 +19,50 @@ module.exports = {
     return await strapi.plugins["users-permissions"].services.jwt.issue({
       id: user.id,
     });
+  },
+  async register(ctx) {
+    const requestBody = ctx.request.body;
+    // validate the request body if it has all the required fields
+    const requiredFields = [
+      "email",
+      "mobile",
+      "firstName",
+      "lastName",
+      "dob",
+      "location",
+      "pincode",
+    ];
+    const missingFields = requiredFields.filter((field) => !requestBody[field]);
+    if (missingFields.length) {
+      ctx.throw(400, `Missing fields: ${missingFields.join(", ")}`);
+    }
+
+    const mobile = requestBody.mobile.match(/\d+/g).join("");
+    const username = uniqid() + uniqid();
+    // const password = pwGenerator.generate({
+    //   length: 12,
+    //   numbers: true,
+    // });
+    const password = requestBody.password || "password";
+    try {
+      const user = await strapi.plugins["users-permissions"].services.user.add({
+        username,
+        email: requestBody.email,
+        mobile,
+        password,
+        firstName: requestBody.firstName,
+        lastName: requestBody.lastName,
+        dob: requestBody.dob,
+        location: requestBody.location,
+        pincode: requestBody.pincode,
+        confirmed: true,
+        blocked: false,
+      });
+      return await strapi.plugins["users-permissions"].services.jwt.issue({
+        id: user.id,
+      });
+    } catch (error) {
+      ctx.throw(400, error);
+    }
   },
 };
