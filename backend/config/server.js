375b995c0588a7a6bb4479a3689b3681522351d0
NeoHBz <neohbz@gmail.com>
2023-01-07 12:01:31 +0530

    init Strapi backend

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4f42810de907284292b79369073d875a'),
+    },
+  },
+});
