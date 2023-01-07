a258d17f08fecba1e0f07b0ca1fb2c25f5f70cf8
janvi01 <janvibajo1@gmail.com>
2023-01-07 10:42:04 +0530

    [frontend]added: navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
 import LandingPage from "./layouts/LandingPage";
 
 function App() {
   return (
     <BrowserRouter>
       <div>
+        <Navbar />
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
